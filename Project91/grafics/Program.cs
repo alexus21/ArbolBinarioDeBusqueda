da55fceec5f06210eda0de671bb1888bc5290978
Alex Ulloa <anders_nielsen_0727@proton.me>
2025-04-07 20:06:31 -0600

    Re-uploading deleted project

---
+++ Project91/grafics/Program.cs
@@ -0,0 +1,29 @@
+﻿using grafics;
+using Project91.GUIForm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArbolBinarioDeBusqueda {
+    internal static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+        //Ejemplo tomado de:
+        //https://stackoverflow.com/questions/32084424/c-sharp-monogame-passing-arguments-on-startup/32086378#32086378
+        [STAThread]
+        static void Main() {
+            var form = new MainForm();
+
+            if (form.ShowDialog() == DialogResult.OK) {
+                using var game = new Game1();
+                    game.Run();
+                Main();
+            }            
+            
+        }
+    }
+}
